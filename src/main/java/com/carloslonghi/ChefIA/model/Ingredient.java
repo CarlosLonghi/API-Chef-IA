5dd795480c908291e8bb2f6c197dcc0784179b9a
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-04-15 23:12:15 -0300

    refactor: update `ingredient` model and `enums`

--- src/main/java/com/carloslonghi/ChefIA/model/Ingredient.java
+++ src/main/java/com/carloslonghi/ChefIA/model/Ingredient.java
@@ -2,16 +2,14 @@ package com.carloslonghi.ChefIA.model;
 
 import com.carloslonghi.ChefIA.model.enums.IngredientCategory;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "ingredient")
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Data
 public class Ingredient {
 
